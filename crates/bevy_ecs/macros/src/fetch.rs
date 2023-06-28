89cb15c4bcf6a511dfa2dac3b43b2d53483c205c
Brian Merchant <bhmerchant@gmail.com>
2023-06-27 20:32:11 -0700

    Trying to use bevy::prelude to get info, as using bevy_utils doesn't work by default

--- crates/bevy_ecs/macros/src/fetch.rs
+++ crates/bevy_ecs/macros/src/fetch.rs
@@ -330,7 +330,7 @@ pub fn derive_world_query_impl(input: TokenStream) -> TokenStream {
                     _entity: #path::entity::Entity,
                     _table_row: #path::storage::TableRow,
                 ) -> bool {
-                    use bevy_utils::tracing::info;
+                    use bevy::prelude::info;
                     info!("Doing some fancy filtering!");
                     true #(&& <#field_types>::filter_fetch(&mut _fetch.#named_field_idents, _entity, _table_row))*
                 }
